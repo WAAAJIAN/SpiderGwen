2c2cb2989e6349a94073754be22077233fd53eac
WAAAJIAN <jianweilim9@gmail.com>
2025-07-10 17:46:18 +0700

    updated all values to 0,0,0

--- ws-code-v1/Spider.py
+++ ws-code-v1/Spider.py
@@ -5,22 +5,22 @@ class Spider:
     def __init__(self):
         self.L1 = Trileg('L1', 
                          sc= 0, sf= 1, st= 2, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         self.L2 = Trileg('L2', 
                          sc= 3, sf= 4, st= 5, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         self.L3 = Trileg('L3', 
                          sc= 6, sf= 7, st= 8, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         self.R1 = Trileg('R1', 
                          sc= 21, sf= 22, st= 23, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         self.R2 = Trileg('R2', 
                          sc= 18, sf= 19, st= 20, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         self.R3 = Trileg('R3', 
                          sc= 9, sf= 10, st= 11, 
-                         coxa_deg= 0, femur_deg= 0, tibia_deg= 0)
+                         coxa_deg= 0, femur_deg= 0, tibia_deg= 180)
         
     def move(self):
         self.L1.move()
@@ -37,12 +37,12 @@ class Spider:
         time.sleep(1)
     
     def clean(self):
-        Spider.L1.clean()
-        Spider.L2.clean()
-        Spider.L3.clean()
-        Spider.R1.clean()
-        Spider.R2.clean()
-        Spider.R3.clean()
+        self.L1.clean()
+        self.L2.clean()
+        self.L3.clean()
+        self.R1.clean()
+        self.R2.clean()
+        self.R3.clean()
         
     ''' gait motion code here '''
 
