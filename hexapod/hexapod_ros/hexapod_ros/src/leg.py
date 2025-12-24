7079e39d280beb9a3e4978a23a76932f9c9b067d
cyanhere <90555087+cyanhere@users.noreply.github.com>
2025-12-24 20:53:33 +0800

    add comments

--- hexapod/hexapod_ros/hexapod_ros/src/leg.py
+++ hexapod/hexapod_ros/hexapod_ros/src/leg.py
@@ -1,22 +1,22 @@
 from .parameter import *
-# from parameter import *
 import traceback
 
 class Leg:
+    """
+    Handles Inverse Kinematics (IK) for a single leg.
+    Translates (X, Y, Z) coordinates into (Coxa, Femur, Tibia) servo angles.
+    """
     def __init__(self, leg):
-        self.leg = leg             # 0 ~ 5, No. of leg, reference to graph on top
-        self.x = x_offset
-        self.y = y_offset
-        self.z = z_offset
-        self.a = 0                 # angle of coxa
-        self.b = 0                # angle of femur
-        self.c = 180              # angle of tibia
-
-        self.offset = 0 if self.leg in (2, 5) else 1
+        self.leg = leg # 0 ~ 5, No. of leg, reference to graph in parameter file
+        # Foot position relative to leg base
+        self.x, self.y, self.z = x_offset, y_offset, z_offset
+        # Joint angles (coxa, femur, tibia)
+        self.a, self.b, self.c = 0, 0, 180
         self.offset_angle = offset_angle_map[self.leg]
         self.IK()
 
     def calculateWalk(self, phase, direction, distance, pitch, roll, yaw, rotate):
+        """ Swing motion, leg tip will draw a semicircle, motion integrated with balance"""
         if phase <= 180:
             turn_distance = 0.5 * distance
             x = direction[0] * (turn_distance * cos(radians(180 - phase)) + turn_distance)
@@ -45,7 +45,7 @@ class Leg:
         self.angleToDC()
         return (self.a, self.b, self.c)
 
-    def stand(self,l,c):
+    def stand(self,l,c): # stand up function (brute force)
         self.IK()
         d = 20 - (20/l)*c
         self.b += d
@@ -53,7 +53,7 @@ class Leg:
         self.angleToDC()
         return (self.a, self.b, self.c)
 
-    def balance(self, pitch, roll): 
+    def balance(self, pitch, roll): # only roll & pitch, no yaw
         R_c = R(self.offset, self.offset_angle, roll, pitch) 
         roll_vec = transformBodyCoortoLeg(self.leg, R_c[0][:2])
         pitch_vec = transformBodyCoortoLeg(self.leg, R_c[1][:2])
@@ -63,40 +63,35 @@ class Leg:
         self.IK()
         self.angleToDC()
         return (self.a, self.b, self.c)
-
+   
     def IK(self):
+        """
+        Inverse Kinematics: 
+        Calculates servo angles using the Law of Cosines based on leg segment lengths (cl, fl, tl).
+        """
         try:
             y = sqrt(self.x**2 + self.y**2) - cl # actual location of tip from femur in coxa frame
             k = sqrt(y**2 + self.z**2) # shortest distance from femur to tibia tip 
             
-            if k < 1e-6:
-                k = 1
-                # raise ValueError("Target too close to femur pivot.")
-            elif k > fl + tl or k < abs(fl - tl):
-                k = fl + tl
-            elif k < abs(fl - tl):
-                k = abs(fl - tl)
-                # raise ValueError("Target out of reachable workspace.")
+            # Constrain k to reachable workspace
+            k = max(abs(fl - tl), min(fl + tl, k))
             
             theta = asin(max(-1, min(1, self.z / k))) # angle between horizontal and k
             theta2 = acos(max(-1, min(1, ((fl**2)+(k**2)-(tl**2))/(2*fl*k)))) # cosine angle of tibia
 
             self.a = max(min(coxa_range[1], degrees(atan2(self.x, self.y))), coxa_range[0]) # coxa angle
             self.b = max(min(femur_range[1], degrees(theta2 - theta)), femur_range[0]) # femur angle
-
             self.c = max(min(tibia_range[1], 180 - degrees(acos(max(-1, min(1, ((fl**2)+(tl**2)-(k**2))/(2*fl*tl)))))), tibia_range[0]) # tibia angle from femur
-            # if self.a > coxa_range[1] or self.a < coxa_range[0] or self.b > femur_range[1] or self.b < femur_range[0] or self.c > tibia_range[1] or self.c < tibia_range[0]:
-                # raise ValueError("Angle exceeded.")
-        except Exception as e:
+        except Exception:
             traceback.print_exc()
-            self.a = 0
-            self.b = 0
-            self.c = 0
+            self.a = self.b = self.c = 0
         
-    def angleToDC(self): # 4000 - 8000
+    def angleToDC(self):
+        """Converts raw degrees to Servo Duty Cycle (PWM) values. (4000 ~ 8000)"""
         self.a = int((100 * self.a)/3 + 6000)
         self.b = int((100 * self.b)/3 + 6000)
         self.c = int((100 * self.c)/3 + 3000)
 
     def curr_angle(self):
         return (self.a, self.b, self.c)
+
