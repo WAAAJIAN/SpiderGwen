1421fe30bc31019262da9114b201bd82bca499f0
WAAAJIAN <jianweilim9@gmail.com>
2026-02-21 01:26:18 +0800

    debugging area for now. Used to read all three assigned servo IDs.

--- firmware/spider_gwen_v4/src/main.cpp
+++ firmware/spider_gwen_v4/src/main.cpp
@@ -1,18 +1,47 @@
 #include <Arduino.h>
+#include "ST3215_driver.h"
 
-// put function declarations here:
-int myFunction(int, int);
+#define SERIAL_BAUD_RATE 1000000
+#define SERIAL_BAUD_RATE_DEBUG 115200
+#define RX 18
+#define TX 17
+
+ST3215_driver servo_driver(Serial1);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial1.begin(SERIAL_BAUD_RATE, SERIAL_8N1, RX, TX);
+    Serial.begin(SERIAL_BAUD_RATE_DEBUG);
+    while (!Serial) {
+        ; // wait for serial port to connect. Needed for native USB
+    }
+
+    Serial.println("Starting ST3215 servo driver test...");
+    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    bool anyResponding = false;
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    if (servo_driver.ping(1))
+    {
+        Serial.println("Servo with ID 1 is present.");
+        anyResponding = true;
+    } 
+    if (servo_driver.ping(2))
+    {
+        Serial.println("Servo with ID 2 is present.");
+        anyResponding = true;
+    } 
+    if (servo_driver.ping(3))
+    {
+        Serial.println("Servo with ID 3 is present.");
+        anyResponding = true;
+    }
+
+    if (!anyResponding)
+    {
+        Serial.println("No servos are responding.");
+    }
+
+    delay(500); // repeat every 500ms
 }
