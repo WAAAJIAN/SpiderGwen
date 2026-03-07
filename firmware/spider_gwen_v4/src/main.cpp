c8da48611e9a7fc8fe601b0238658ce5d4a5f4ed
WAAAJIAN <jianweilim9@gmail.com>
2026-03-07 14:59:29 +0800

    example(main): showing example of instantiation of driver

--- firmware/spider_gwen_v4/src/main.cpp
+++ firmware/spider_gwen_v4/src/main.cpp
@@ -1,3 +1,6 @@
+
+
+
 #include <Arduino.h>
 #include "ST3215_driver.h"
 
@@ -6,42 +9,56 @@
 #define RX 18
 #define TX 17
 
-ST3215_driver servo_driver(Serial1);
+ST3215_driver *servo_driver;
 
-void setup() {
-    Serial1.begin(SERIAL_BAUD_RATE, SERIAL_8N1, RX, TX);
-    Serial.begin(SERIAL_BAUD_RATE_DEBUG);
-    while (!Serial) {
-        ; // wait for serial port to connect. Needed for native USB
+void setup() 
+{
+  servo_driver = new ST3215_driver(Serial1);
+  Serial1.begin(SERIAL_BAUD_RATE, SERIAL_8N1, RX, TX);
+  Serial.begin(SERIAL_BAUD_RATE_DEBUG);
+
+  while (!Serial) 
+  {
+    ; // wait for serial port to connect. Needed for native USB
+  }
+
+  Serial.println("Starting ST3215 servo driver test...");
+
+  for (uint8_t id = 1; id <= 3; id++) 
+  {
+    uint8_t load;
+    size_t speed;
+    if (servo_driver->ping(id)) 
+    {
+
+      Serial.print("Servo with ID ");
+      Serial.print(id);
+      Serial.println(" is present.");
+      servo_driver->set_speed(id, 200);
+      servo_driver->set_angle(id, 180);
+      servo_driver->read_load(id, &load);
+      servo_driver->read_speed(id, &speed);
+      Serial.print("Current load: ");
+      Serial.print(load);
+      Serial.print(", Current speed: ");
+      Serial.println(speed);
+    } 
+    else
+    {
+      Serial.print("Servo with ID ");
+      Serial.print(id);
+      Serial.println(" is not responding.");
     }
+  }
 
-    Serial.println("Starting ST3215 servo driver test...");
-    
 }
 
 void loop() {
-    bool anyResponding = false;
-
-    if (servo_driver.ping(1))
-    {
-        Serial.println("Servo with ID 1 is present.");
-        anyResponding = true;
-    } 
-    if (servo_driver.ping(2))
-    {
-        Serial.println("Servo with ID 2 is present.");
-        anyResponding = true;
-    } 
-    if (servo_driver.ping(3))
-    {
-        Serial.println("Servo with ID 3 is present.");
-        anyResponding = true;
-    }
-
-    if (!anyResponding)
-    {
-        Serial.println("No servos are responding.");
-    }
-
-    delay(500); // repeat every 500ms
+  // for (uint8_t id = 1; id <= 3; id++) 
+  // {
+  //   servo_driver.set_angle(id, 40);
+  //   delay(1000);
+  //   servo_driver.set_angle(id, 0);
+  //   delay(1000);
+  // }
 }
